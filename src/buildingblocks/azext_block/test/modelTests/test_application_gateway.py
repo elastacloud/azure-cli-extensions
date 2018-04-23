5eab6ce7da1d87673453f622b01b20e0a910cd6c
Andy Cross <andy@elastacloud.com>
2018-04-23 18:28:17 +0100

    frontendipconfiguration and sku tests

--- src/buildingblocks/azext_block/test/modelTests/test_application_gateway.py
+++ src/buildingblocks/azext_block/test/modelTests/test_application_gateway.py
@@ -1,21 +1,34 @@
+from enum import Enum
 import unittest
-from unittest.mock import MagicMock
-from azext_block.models import (ApplicationGatewayBuildingBlock, ApplicationGateway)
+import unittest.mock as mock
 
-class ApplicationGatewayTest(unittest.TestCase):
-    _staticRG = frozenset(["resourceGroup1", "resourceGroup2"])
-    _settings = [ApplicationGateway(subscription_id="b33f13", frontend_ip_configurations=[
-        {
-            "frontendIPConfigurations": [{ 
-                "name" : "sampleIPConfig", 
-                "applicationGatewayType": "Public" 
-            }]
-    }])]
+from azext_block.models import (Sku, ApplicationGatewayBuildingBlock, ApplicationGateway, FrontendIPConfiguration)
+   
+class FrontendIPConfigurationTest(unittest.TestCase):
+    def test_is_Public_valid_gateway_type(self):
+        target = FrontendIPConfiguration(application_gateway_type='Public')
+        self.assertTrue(target._is_valid_gateway_type("Public"))
 
-    def setUp(self):
-        self.target = ApplicationGatewayBuildingBlock(self._settings)
-        self.target.proxy_extract_resource_groups = MagicMock(return_value=self._staticRG)
+    def test_is_Private_valid_gateway_type(self):
+        target = FrontendIPConfiguration(application_gateway_type='Internal')
+        self.assertTrue(target._is_valid_gateway_type("Internal"))
 
-    def test_transform_uses_settings(self):
-        resultRG, _ = self.target.transform()
-        self.assertCountEqual(resultRG, self._staticRG)
+    def test_is_Frog_invalid_gateway_type(self):        
+        target = FrontendIPConfiguration(application_gateway_type='Frog')
+        self.assertFalse(target._is_valid_gateway_type("Frog"))
+
+    def test_is_valid_gateway_type_irrelevant_to_current(self):        
+        target = FrontendIPConfiguration(application_gateway_type='Internal')
+        self.assertTrue(target._is_valid_gateway_type("Public"))
+
+
+class MockSkus(Enum):
+    small = 'Standard_Big'
+
+class mockingNamespaces(unittest.TestCase):
+    @mock.patch('azext_block.models.Sku._valid_sizes', new_callable=mock.PropertyMock)
+    def test_with_cstr(self, mocked_p):
+        mocked_p.return_value = ['Standard_Big', 'Standard_Mocks']
+        target = Sku()
+        self.assertTrue(target._is_valid_sku("Big"))
+
